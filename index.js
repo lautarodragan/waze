7bbe56e9916dd3deea165c1d5f93bdff92e2d73b
taro <lautaro@airtm.io>
2022-09-10 21:53:46 -0300

    render data, all same color, only first sample

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const canvas = document.getElementsByTagName('canvas')[0]
   const canvasContext = canvas.getContext('2d')
+  const { trafficMeasurements } = await loadTransitSamples()
+
+  // console.log('transitSamples', trafficMeasurements)
+
+  const blockSize = 30
 
   const render = () => {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height)
-    canvasContext.fillRect(50, 50, 500, 500)
+    // canvasContext.scale(blockSize, blockSize)
+
+    const [ trafficMeasurement ] = trafficMeasurements
+
+    console.log('trafficMeasurement', trafficMeasurement)
+
+    canvasContext.beginPath()
+
+    for (const measurement of trafficMeasurement.measurements) {
+      const startAvenueIndex = measurement.startAvenue.charCodeAt(0) - 65
+      const endAvenueIndex = measurement.endAvenue.charCodeAt(0) - 65
+      const startStreetIndex = parseInt(measurement.startStreet)
+      const endStreetIndex = parseInt(measurement.endStreet)
+
+      canvasContext.moveTo(startAvenueIndex * blockSize, startStreetIndex * blockSize)
+      canvasContext.lineTo(endAvenueIndex * blockSize, endStreetIndex * blockSize)
+    }
+
+    canvasContext.stroke()
   }
 
   const setCanvasSize = () => {
@@ -23,3 +45,4 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 })
 
+const loadTransitSamples = () => fetch('/sample-data.json').then(_ => _.json())
