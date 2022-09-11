325b2db2e894bd4a34b88bda3806ea761f3278d7
taro <lautaro@airtm.io>
2022-09-10 22:18:22 -0300

    render transit time in red + some logging

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ document.addEventListener('DOMContentLoaded', async () => {
   const canvasContext = canvas.getContext('2d')
   const { trafficMeasurements } = await loadTransitSamples()
 
-  // console.log('transitSamples', trafficMeasurements)
+  console.log('transitSamples', trafficMeasurements)
+
+  const minTransitTimes = trafficMeasurements.map(_ => _.measurements.reduce(min, 0))
+  const maxTransitTimes = trafficMeasurements.map(_ => _.measurements.reduce(max, 0))
+
+  console.log('min transit times', minTransitTimes)
+  console.log('max transit times', maxTransitTimes)
 
   const blockSize = 30
 
   const render = () => {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height)
     // canvasContext.scale(blockSize, blockSize)
+    canvasContext.translate(200, 0)
 
     const [ trafficMeasurement ] = trafficMeasurements
+    const [ maxTransitTime ] = maxTransitTimes
 
     console.log('trafficMeasurement', trafficMeasurement)
-
-    canvasContext.beginPath()
+    console.log('maxTransitTime', maxTransitTime)
 
     for (const measurement of trafficMeasurement.measurements) {
       const startAvenueIndex = measurement.startAvenue.charCodeAt(0) - 65
@@ -23,11 +30,16 @@ document.addEventListener('DOMContentLoaded', async () => {
       const startStreetIndex = parseInt(measurement.startStreet)
       const endStreetIndex = parseInt(measurement.endStreet)
 
+      const red = Math.floor(measurement.transitTime / maxTransitTime * 256)
+      // console.log('red', red)
+
+      canvasContext.beginPath()
+      canvasContext.strokeStyle = `rgb(${red}, 0, 0)`
       canvasContext.moveTo(startAvenueIndex * blockSize, startStreetIndex * blockSize)
       canvasContext.lineTo(endAvenueIndex * blockSize, endStreetIndex * blockSize)
+      canvasContext.stroke()
     }
 
-    canvasContext.stroke()
   }
 
   const setCanvasSize = () => {
@@ -46,3 +58,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 })
 
 const loadTransitSamples = () => fetch('/sample-data.json').then(_ => _.json())
+
+const max = (acc, curr) => curr.transitTime > acc ? curr.transitTime : acc
+const min = (acc, curr) => curr.transitTime < acc ? curr.transitTime : acc
